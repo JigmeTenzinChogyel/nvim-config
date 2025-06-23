dbef4c251cb9089a18a19c961db46a437e2ecf49
JigmeTenzinChogyel <jigmetenzinchogyel@gmail.com>
2025-06-23 15:29:43 +0600

    tree sitter

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,6 +17,11 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,16 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  branch = "master",
+  lazy = false,
+  build = ":TSUpdate",
+  config = function()
+    require('nvim-treesitter.configs').setup({
+      ensure_installed = { "lua", "go", "javascript", "typescript" }, -- languages to install
+      highlight = { enable = true },   -- enable syntax highlighting
+      indent = { enable = true },      -- enable indentation based on treesitter
+      incremental_selection = { enable = true },
+      -- add more features here as needed
+    })
+  end,
+}
+
