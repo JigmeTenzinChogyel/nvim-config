66cf5b57fc6afc29c829dc444710755add96885f
JigmeTenzinChogyel <jigmetenzinchogyel@gmail.com>
2025-06-25 09:40:42 +0600

    terminal

---
+++ lua/plugins/terminal.lua
@@ -0,0 +1,23 @@
+return {
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	config = function()
+		require("toggleterm").setup({
+			size = function(term)
+				if term.direction == "horizontal" then
+					return 15
+				elseif term.direction == "vertical" then
+					return vim.o.columns * 0.4
+				end
+			end,
+			open_mapping = [[<c-\>]],
+			insert_mappings = true,
+			terminal_mappings = true,
+			persist_size = true,
+			direction = "horizontal", -- default direction
+			close_on_exit = true,
+			shell = vim.o.shell,
+			auto_scroll = true,
+		})
+	end,
+}
