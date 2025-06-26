7fa13e1fe9ac805ac8d5696757fca4586de19b96
JigmeTenzinChogyel <jigmetenzinchogyel@gmail.com>
2025-06-26 10:45:53 +0600

    auto close tags

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,17 +1,38 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  branch = "master",
-  lazy = false,
-  build = ":TSUpdate",
-  config = function()
-    require('nvim-treesitter.configs').setup({
-      auto_install = true,
-      ensure_installed = { "lua", "go", "javascript", "typescript" }, -- languages to install
-      highlight = { enable = true },   -- enable syntax highlighting
-      indent = { enable = true },      -- enable indentation based on treesitter
-      incremental_selection = { enable = true },
-      -- add more features here as needed
-    })
-  end,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "master",
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+      require('nvim-treesitter.configs').setup({
+        auto_install = true,
+        ensure_installed = { "lua", "go", "javascript", "typescript", "html", "tsx", "vue" },
+        highlight = { enable = true },
+        indent = { enable = true },
+        incremental_selection = { enable = true },
+      })
+    end,
+  },
+
+  {
+    "windwp/nvim-ts-autotag",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      require("nvim-ts-autotag").setup({
+        opts = {
+          enable_close = true,
+          enable_rename = true,
+          enable_close_on_slash = false,
+        },
+        per_filetype = {
+          html = {
+            enable_close = false, -- example override
+          },
+        },
+      })
+    end,
+  },
 }
 
